ab5a24b777d59d2c154e29f1b4b9fe7e6c9d3761
Liang-li,Huang <liang-li.huang@hpe.com>
2016-12-09 16:40:34 +0800

    User login feature

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -8,6 +8,28 @@
 
 $(function() {
     var apiPath = "http://118.178.124.197:8080/";
+    var  usrInvalid = function(){
+
+        //check if both key userName and timeStamp exist
+        if(null === sessionStorage.getItem("userName") || null === sessionStorage.getItem("timeStamp")){
+            return true;
+        }
+
+        //check login expired or not
+        var checkResult = false;
+        var now = new Date();
+        var expiration = new Date(sessionStorage.getItem("timeStamp"));
+        expiration.setMinutes(expiration.getMinutes() + 30);
+
+        // ditch the content if too old
+        if (now.getTime() > expiration.getTime()) {
+            sessionStorage.removeItem("userName");
+            sessionStorage.removeItem("timeStamp");
+            checkResult = true;
+        }
+
+        return checkResult;
+    };
     var loading = function(){
         var loadScreenDiv = $("#loadingScreen");
         var loadingScreenLen = loadScreenDiv.width();
@@ -73,6 +95,14 @@ $(function() {
     }]);
     app.config(function($routeProvider) {
         $routeProvider
+            .when("/login", {
+                templateUrl : "app/views/common/login.html",
+                controller : "loginCtrl"
+            })
+            .when("/logout", {
+                templateUrl : "app/views/common/login.html",
+                controller : "logoutCtrl"
+            })
             .when("/primary_product", {
                 templateUrl : "app/views/product/primary_product.html",
                 controller : "primaryProductCtrl"
@@ -123,6 +153,11 @@ $(function() {
             });
     });
     app.controller("primaryProductCtrl", function ($scope, $http) {
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         //get product list data
         $scope.productItems = [];
         $scope.layoutUpdate = {"entities": []};
@@ -241,6 +276,11 @@ $(function() {
         };
     });
     app.controller("productListCtrl", function ($scope, $http, productBackAction){
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         //parse product category id to name
         var parseProductCategoryItem = function(){
             angular.forEach($scope.productItems, function(product){
@@ -417,6 +457,11 @@ $(function() {
             });
     });
     app.controller("updateARProductDetail", function ($scope, $http, $routeParams, productBackAction) {
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         console.log("Product ID: "+ $routeParams.productID);
         $scope.firstScreenShot = "";
         $scope.secondScreenShot = "";
@@ -667,6 +712,11 @@ $(function() {
         };
     });
     app.controller("newARDetailCtrl", function ($scope, $http, productBackAction) {
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         //new product info
         $scope.productInfo = {};
         $scope.firstScreenShot = ""
@@ -875,6 +925,10 @@ $(function() {
         };
     });
     app.controller("updateVideoDetailCtrl", function($scope, $http, $routeParams, productBackAction){
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
 
         $scope.productCategory = [];
 
@@ -1042,6 +1096,11 @@ $(function() {
         };
     });
     app.controller("newVideoDetailCtrl", function ($scope, $http, productBackAction) {
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         //new product info
         $scope.productInfo = {};
         $scope.productInfo.videoDOs = [];
@@ -1176,6 +1235,10 @@ $(function() {
         };
     });
     app.controller("firstLevelCategoryCtrl", function ($scope,$http){
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
 
         $scope.firstLevelCategoryItems = null;
         loading();
@@ -1291,6 +1354,11 @@ $(function() {
         };
     });
     app.controller("userAdminCtrl", function ($scope, $http){
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         $scope.userItems = null;
         loading();
 
@@ -1337,6 +1405,11 @@ $(function() {
         };
     });
     app.controller("logDownloadCtrl", function ($scope, $http){
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         $scope.logItems = null;
         $scope.allItemsLength = 0;
         $scope.currentPage = 0;
@@ -1400,6 +1473,12 @@ $(function() {
         }
     });
     app.controller("systemInfoCtrl", function ($scope, $http){
+
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
+
         $scope.systemInfo = {};
         $scope.systemInfo.systemLatestVersion = "";
         $scope.systemInfo.systemApkDownUrl = "";
@@ -1464,8 +1543,27 @@ $(function() {
                 });
         };
     });
+    app.controller("loginCtrl", function ($scope, $http) {
+        $scope.userLogin = function(){
+            console.log("$scope.userName");
+            console.log("$scope.userPassword");
+
+            //credential verification and set both userName and timeStamp
+            sessionStorage.setItem("userName","admin");
+            sessionStorage.setItem("timeStamp",new Date());
+        };
+    });
+    app.controller("logoutCtrl", function ($scope, $http) {
+        sessionStorage.removeItem("userName");
+        sessionStorage.removeItem("timeStamp");
+        window.location.href = "#login/";
+    });
     app.controller("otherUrlCtrl", function () {
         console.log("Otherwise URL contoller...");
+        if(usrInvalid()){
+            window.location.href = "#login/";
+            return;
+        }
     });
 }());
 
