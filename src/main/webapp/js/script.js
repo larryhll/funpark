060aedaa731f3819de7cf16f14af071dccd8fdc6
Liang-li,Huang <liang-li.huang@hpe.com>
2016-12-10 18:57:37 +0800

    refine product pages

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -154,6 +154,22 @@
 * FUNP - v1.0.0 
 * © Copyright 2016  Liangli Huang
  */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
 /**
  * Created by hualiang on 16-11-20.
  */
